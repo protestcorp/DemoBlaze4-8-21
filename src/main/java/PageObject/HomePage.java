0db15844108dc3e87a00313bb267da1e38e31e6e
protestcorp <85497328+protestcorp@users.noreply.github.com>
2021-10-18 14:02:27 +0530

    Update HomePage.java

--- src/main/java/PageObject/HomePage.java
+++ src/main/java/PageObject/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import Actions.ActionUtilities;
 
-import CucumberOptionss.PageObjectHandler;
+import CucumberOptionsss.PageObjectHandler;
 import net.serenitybdd.core.Serenity;
 import net.thucydides.core.annotations.Managed;
 
